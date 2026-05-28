13e10878975b3438d9ef996365334ff943851a40
publiceye33 <publiceye33@gmail.com>
2026-05-28 17:50:16 +0600

    refactor: remove temporary repository files
    Delete all files and configurations associated with the _temp_repo directory to clean up the project structure.

--- _temp_repo/temp_repo/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import next from "eslint-config-next";
-
-export default [
-  ...next.configs.recommended,
-  ...next.configs["core-web-vitals"].rules,
-  {
-    ignores: [".next/*", "dist/*"],
-  },
-];
